0fcc8c7fe550ca48deedc729cc5a106b014c5a31
Candidus <candidus@xmail.net>
2023-01-15 19:10:03 +0100

    Remove BOMs, change line endings to LF.

--- LittleNPCs/Framework/Patches/NPCArriveAtFarmHousePatch.cs
+++ LittleNPCs/Framework/Patches/NPCArriveAtFarmHousePatch.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using StardewValley;
 using StardewValley.Locations;
 using StardewValley.Network;
--- LittleNPCs/Framework/Patches/NPCParseMasterSchedulePatch.cs
+++ LittleNPCs/Framework/Patches/NPCParseMasterSchedulePatch.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using StardewValley;
 using StardewModdingAPI;
 using System;
