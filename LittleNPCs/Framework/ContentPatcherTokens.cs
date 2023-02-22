33879bd658f127aac67f4f44c10220e73c42655c
Candidus <candidus@xmail.net>
2023-02-22 20:22:10 +0100

    Extract methods to simplify token implementation.

--- LittleNPCs/Framework/ContentPatcherTokens.cs
+++ LittleNPCs/Framework/ContentPatcherTokens.cs
@@ -1,6 +1,8 @@
 using System.Linq;
 using System.Collections.Generic;
 
+using StardewModdingAPI;
+
 
 namespace LittleNPCs.Framework {
     internal static class ContentPatcherTokens {
@@ -11,82 +13,58 @@ namespace LittleNPCs.Framework {
                 var api = modEntry.Helper.ModRegistry.GetApi<ContentPatcher.IContentPatcherAPI>("Pathoschild.ContentPatcher");
 
                 api.RegisterToken(modEntry.ModManifest, "FirstLittleNPCName", () => {
-                    var littleNPC = new LittleNPCInfo(0, modEntry.Monitor);
-                    if (littleNPC is not null && !littleNPC.Equals(cachedLittleNPCs_[0])) {
-                        cachedLittleNPCs_[0] = littleNPC;
-
-                        modEntry.Monitor.Log($"FirstLittleNPCName() returns {cachedLittleNPCs_[0].Name}");
-
-                        return cachedLittleNPCs_[0].Name.ToTokenReturnValue();
-                    }
+                    UpdateFirstLittleNPC(modEntry.Monitor);
 
                     return cachedLittleNPCs_[0].Name.ToTokenReturnValue();
                 });
 
                 api.RegisterToken(modEntry.ModManifest, "FirstLittleNPCDisplayName", () => {
-                    var littleNPC = new LittleNPCInfo(0, modEntry.Monitor);
-                    if (littleNPC is not null && !littleNPC.Equals(cachedLittleNPCs_[0])) {
-                        cachedLittleNPCs_[0] = littleNPC;
-
-                        modEntry.Monitor.Log($"FirstLittleNPCDisplayName() returns {cachedLittleNPCs_[0].DisplayName}");
-
-                        return cachedLittleNPCs_[0].DisplayName.ToTokenReturnValue();
-                    }
+                    UpdateFirstLittleNPC(modEntry.Monitor);
 
                     return cachedLittleNPCs_[0].DisplayName.ToTokenReturnValue();
                 });
 
                 api.RegisterToken(modEntry.ModManifest, "FirstLittleNPCGender", () => {
-                    var littleNPC = new LittleNPCInfo(0, modEntry.Monitor);
-                    if (littleNPC is not null && !littleNPC.Equals(cachedLittleNPCs_[0])) {
-                        cachedLittleNPCs_[0] = littleNPC;
-
-                        modEntry.Monitor.Log($"FirstLittleNPCGender() returns {cachedLittleNPCs_[0].Gender}");
-
-                        return cachedLittleNPCs_[0].Gender.ToTokenReturnValue();
-                    }
+                    UpdateFirstLittleNPC(modEntry.Monitor);
 
                     return cachedLittleNPCs_[0].Gender.ToTokenReturnValue();
                 });
 
                 api.RegisterToken(modEntry.ModManifest, "SecondLittleNPCName", () => {
-                    var littleNPC = new LittleNPCInfo(1, modEntry.Monitor);
-                    if (littleNPC is not null && !littleNPC.Equals(cachedLittleNPCs_[1])) {
-                        cachedLittleNPCs_[1] = littleNPC;
-
-                        modEntry.Monitor.Log($"SecondLittleNPCName() returns {cachedLittleNPCs_[1].Name}");
-
-                        return cachedLittleNPCs_[1].Name.ToTokenReturnValue();
-                    }
+                    UpdateSecondLittleNPC(modEntry.Monitor);
 
                     return cachedLittleNPCs_[1].Name.ToTokenReturnValue();
                 });
 
                 api.RegisterToken(modEntry.ModManifest, "SecondLittleNPCDisplayName", () => {
-                    var littleNPC = new LittleNPCInfo(1, modEntry.Monitor);
-                    if (littleNPC is not null && !littleNPC.Equals(cachedLittleNPCs_[1])) {
-                        cachedLittleNPCs_[1] = littleNPC;
-
-                        modEntry.Monitor.Log($"SecondLittleNPCDisplayName() returns {cachedLittleNPCs_[1].DisplayName}");
-
-                        return cachedLittleNPCs_[1].DisplayName.ToTokenReturnValue();
-                    }
+                    UpdateSecondLittleNPC(modEntry.Monitor);
 
                     return cachedLittleNPCs_[1].DisplayName.ToTokenReturnValue();
                 });
 
                 api.RegisterToken(modEntry.ModManifest, "SecondLittleNPCGender", () => {
-                    var littleNPC = new LittleNPCInfo(1, modEntry.Monitor);
-                    if (littleNPC is not null && !littleNPC.Equals(cachedLittleNPCs_[1])) {
-                        cachedLittleNPCs_[1] = littleNPC;
-
-                        modEntry.Monitor.Log($"SecondLittleNPCGender() returns {cachedLittleNPCs_[1].Gender}");
-
-                        return cachedLittleNPCs_[1].Gender.ToTokenReturnValue();
-                    }
+                    UpdateSecondLittleNPC(modEntry.Monitor);
 
                     return cachedLittleNPCs_[1].Gender.ToTokenReturnValue();
                 });
+            }
+
+            private void UpdateFirstLittleNPC(IMonitor monitor) {
+                var littleNPC = new LittleNPCInfo(0, monitor);
+                if (littleNPC is not null && !littleNPC.Equals(cachedLittleNPCs_[0])) {
+                    cachedLittleNPCs_[0] = littleNPC;
+
+                    monitor.Log($"FirstLittleNPC updated: {cachedLittleNPCs_[0]}");
+                }
+            }
+
+            private void UpdateSecondLittleNPC(IMonitor monitor) {
+                var littleNPC = new LittleNPCInfo(1, monitor);
+                if (littleNPC is not null && !littleNPC.Equals(cachedLittleNPCs_[1])) {
+                    cachedLittleNPCs_[1] = littleNPC;
+
+                    monitor.Log($"SecondLittleNPC updated: {cachedLittleNPCs_[1]}");
+                }
             }
         }
 
