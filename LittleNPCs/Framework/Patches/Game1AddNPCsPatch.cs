498e786a38c99cd286ea69ed88068738ed6081d2
Candidus <30182778+mus-candidus@users.noreply.github.com>
2025-09-25 21:18:27 +0200

    Change code style and class scope for consistency.

--- LittleNPCs/Framework/Patches/Game1AddNPCsPatch.cs
+++ LittleNPCs/Framework/Patches/Game1AddNPCsPatch.cs
@@ -1,18 +1,18 @@
 using StardewValley;
 
-namespace LittleNPCs.Framework.Patches;
 
-
-public static class Game1AddNPCsPatch {
-    /// <summary>
-    /// Prefix for <code>Game1.AddNPCs</code>.
-    /// Unsets SpawnIfMissing for every NPC that has a LittleNPC internal asset name to prevent ghost NPC copies.
-    /// </summary>
-    public static void Prefix() {
-        foreach (var item in Game1.characterData) {
-            if (Common.IsLittleNPCInternalAssetName(item.Key) && item.Value.SpawnIfMissing) {
-                item.Value.SpawnIfMissing = false;
-                ModEntry.monitor_.Log($"Set SpawnIfMissing of {item.Key} to false.");
+namespace LittleNPCs.Framework.Patches {
+    class Game1AddNPCsPatch {
+        /// <summary>
+        /// Prefix for <code>Game1.AddNPCs</code>.
+        /// Unsets SpawnIfMissing for every NPC that has a LittleNPC internal asset name to prevent ghost NPC copies.
+        /// </summary>
+        public static void Prefix() {
+            foreach (var item in Game1.characterData) {
+                if (Common.IsLittleNPCInternalAssetName(item.Key) && item.Value.SpawnIfMissing) {
+                    item.Value.SpawnIfMissing = false;
+                    ModEntry.monitor_.Log($"Set SpawnIfMissing of {item.Key} to false.");
+                }
             }
         }
     }
