425038e8ee21867beab5bc6cb3e34bfc77eb3fa4
Candidus <30182778+mus-candidus@users.noreply.github.com>
2026-05-19 20:52:19 +0200

    Revert "Change log level to Trace so verbose logging is not required anymore."
    The log level change was useful to find a long-standing bug but generates too many log messages for normal use.
    
    This reverts commit 50cc4216dee859a4a05eb8c61847af6d2194803d.

--- LittleNPCs/Framework/LittleNPCInfo.cs
+++ LittleNPCs/Framework/LittleNPCInfo.cs
@@ -33,7 +33,7 @@ namespace LittleNPCs.Framework {
                     Gender = littleNPC.Gender;
                     Birthday = littleNPC.GetBirthday();
                     LoadedFrom = LoadState.LittleNPC;
-                    ModEntry.monitor_.Log($"[{Common.GetHostTag()}] GetLittleNPC({childIndex}) returns {this}", LogLevel.Trace);
+                    ModEntry.monitor_.VerboseLog($"[{Common.GetHostTag()}] GetLittleNPC({childIndex}) returns {this}");
                 }
                 else {
                     // No LittleNPC, try to get Child object.
@@ -55,10 +55,10 @@ namespace LittleNPCs.Framework {
                 info.Gender = child.Gender;
                 info.Birthday = Common.GetBirthday(child, loadFromSave);
                 info.LoadedFrom = loadFromSave ? LoadState.SaveGame : LoadState.Child;
-                ModEntry.monitor_.Log($"[{Common.GetHostTag()}] Query for convertible child with index {childIndex} returns {info}", LogLevel.Trace);
+                ModEntry.monitor_.VerboseLog($"[{Common.GetHostTag()}] Query for convertible child with index {childIndex} returns {info}");
             }
             else {
-                ModEntry.monitor_.Log($"[{Common.GetHostTag()}] Query for convertible child with index {childIndex} returns null", LogLevel.Trace);
+                ModEntry.monitor_.VerboseLog($"[{Common.GetHostTag()}] Query for convertible child with index {childIndex} returns null");
             }
         }
 
